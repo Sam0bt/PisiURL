1848ea1041c70415af79331d495f369179505f6f
Sami Babat <sami.babat@gmail.com>
2016-06-11 02:02:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 chmod 777 -R PisiURL.sh && cp PisiURL.sh -a /var/opt/ &&
 cp Bakkal.desktop -a /usr/share/applications &&
-gconftool-2 -t string -s /desktop/gnome/url-handlers/pisix/command "/usr/share/applications/Bakkal.desktop %U" &&
-gconftool-2 -s /desktop/gnome/url-handlers/pisix/needs_terminal false -t bool &&
-gconftool-2 -t bool -s /desktop/gnome/url-handlers/pisix/enabled true &&
+gconftool-2 -t string -s /desktop/gnome/url-handlers/pisi/command "/usr/share/applications/Bakkal.desktop %U" &&
+gconftool-2 -s /desktop/gnome/url-handlers/pisi/needs_terminal false -t bool &&
+gconftool-2 -t bool -s /desktop/gnome/url-handlers/pisi/enabled true &&
 sudo update-desktop-database
 
 
