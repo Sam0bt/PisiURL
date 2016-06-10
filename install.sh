2d34019b1bb41d9b803211767963bfdbf1d9f5e8
Sami Babat <sami.babat@gmail.com>
2016-06-11 02:01:54 +0300

    install.sh
    install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+chmod 777 -R PisiURL.sh && cp PisiURL.sh -a /var/opt/ &&
+cp Bakkal.desktop -a /usr/share/applications &&
+gconftool-2 -t string -s /desktop/gnome/url-handlers/pisix/command "/usr/share/applications/Bakkal.desktop %U" &&
+gconftool-2 -s /desktop/gnome/url-handlers/pisix/needs_terminal false -t bool &&
+gconftool-2 -t bool -s /desktop/gnome/url-handlers/pisix/enabled true &&
+sudo update-desktop-database
+
+
