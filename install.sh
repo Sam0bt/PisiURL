837a3e43261f6f919bb589519c022dfeecd8440e
Sami BABAT <sami.babat@gmail.com>
2020-05-24 21:01:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cp  PisiURL.sh /var/opt
-cp bakkal.desktop /usr/share/applications/
-
 chmod 777 /var/opt/PisiURL.sh
 xdg-mime default /usr/share/applications/Bakkal.desktop  x-scheme-handler/pisi &&
 sudo update-desktop-database
